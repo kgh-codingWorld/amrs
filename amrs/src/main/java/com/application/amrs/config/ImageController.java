996ed94172242cc93b3407368f1c750755a81311
kgh-codingWorld <tto4028@naver.com>
2025-02-14 23:38:05 +0900

    프로필 이미지 처리

---
+++ amrs/src/main/java/com/application/amrs/config/ImageController.java
@@ -0,0 +1,22 @@
+package com.application.amrs.config;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class ImageController {
+
+	@Value("${spring.web.resources.static-locations}")
+	private String FILE_DIRECTORY;
+
+    @GetMapping("/{filename}")
+    public Resource getFile(@PathVariable String filename) throws Exception {
+        Path filePath = Paths.get(FILE_DIRECTORY).resolve(filename).normalize();
+        return new UrlResource(filePath.toUri());
+    }
+}
